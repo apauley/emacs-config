c30cd43382eea8dd21a083ccbe802291d22d5ffd
Andreas Pauley <apauley+github@gmail.com>
2014-03-23 23:38:02 +0200

    Add custom.el

---
+++ .emacs.d/custom.el
@@ -0,0 +1,17 @@
+(column-number-mode 1)
+(global-linum-mode 1)
+
+;; Recent Files
+(setq recentf-auto-cleanup 'never) ;; disable before we start recentf!
+(recentf-mode 1)
+(setq recentf-max-menu-items 25)
+(global-set-key "\C-x\ f" 'recentf-open-files)
+
+(global-set-key (kbd "C-<right>") 'windmove-right)
+(global-set-key (kbd "C-<left>") 'windmove-left)
+
+(add-hook 'before-save-hook 'delete-trailing-whitespace)
+(setq whitespace-style
+      '(trailing lines space-before-tab indentation space-after-tab)
+      whitespace-line-column 200)
+(global-whitespace-mode)
