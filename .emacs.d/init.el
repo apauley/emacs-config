26b69143b3d8c7ddf963fce9f65dbc8f644dcbb3
Andreas Pauley <apauley+github@gmail.com>
2014-03-23 22:07:29 +0200

    Use init.el from http://tim.dysinger.net/posts/2014-02-18-haskell-with-emacs.html

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -5,40 +5,24 @@
 ;; NO JUNK
 (setq auto-save-file-name-transforms `((".*" ,temporary-file-directory t))
       backup-directory-alist `((".*" . ,temporary-file-directory)))
-
 ;; EL-GET
-(add-to-list 'load-path "~/.emacs.d/el-get/el-get")
-
+(add-to-list 'load-path (locate-user-emacs-file "el-get/el-get"))
 (unless (require 'el-get nil 'noerror)
   (with-current-buffer
       (url-retrieve-synchronously
        "https://raw.github.com/dimitri/el-get/master/el-get-install.el")
     (goto-char (point-max))
     (eval-print-last-sexp)))
-
-(add-to-list 'el-get-recipe-path "~/.emacs.d/el-get-user/recipes")
-(add-to-list 'el-get-recipe-path "~/.emacs.d/el-get-haskell/recipes")
-(el-get 'sync)
-
-;; Other
-(require 'package)
-(add-to-list 'package-archives
-             '("marmalade" . "http://marmalade-repo.org/packages/") t)
-(package-initialize)
-
-(when (not package-archive-contents)
-  (package-refresh-contents))
-
-(defvar common-packages '(better-defaults)
-  "A list of common packages to ensure are installed at launch.")
-
-(defvar ui-packages '(color-theme color-theme-solarized color-theme-monokai color-theme-molokai)
-  "A list of UI packages to ensure are installed at launch.")
-
-(defvar packages (append common-packages ui-packages)
-  "Combined list of packes to be installed")
-
-;; (dolist (p packages)
-;;   (when (not (package-installed-p p))
-;;     (message "package not installed: %s" p)
-;;     (package-install p)))
+(defun el-get-sync-recipes (overlay)
+  (let* ((recipe-glob (locate-user-emacs-file (concat overlay "/recipes/*.rcp")))
+         (recipe-files (file-expand-wildcards recipe-glob))
+         (recipes (mapcar 'el-get-read-recipe-file recipe-files)))
+    (mapcar (lambda (r) (add-to-list 'el-get-sources r)) recipes)
+    (el-get 'sync (mapcar 'el-get-source-name recipes))))
+(setq el-get-user-package-directory user-emacs-directory)
+;; EL-GET SYNC OVERLAYS
+(el-get-sync-recipes "el-get-haskell")
+(el-get-sync-recipes "el-get-user")
+;; CUSTOM FILE
+(setq custom-file (locate-user-emacs-file "custom.el"))
+(load custom-file 'noerror)
