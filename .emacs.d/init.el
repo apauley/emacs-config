c7d3df8c9bca43e1fb6b2e0d0509f4cd1785b035
Andreas Pauley <apauley+github@gmail.com>
2014-03-23 21:25:01 +0200

    Add el-get

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,3 +1,25 @@
+;; NO FRILLS
+(dolist (mode '(menu-bar-mode tool-bar-mode scroll-bar-mode))
+  (when (fboundp mode) (funcall mode -1)))
+(setq inhibit-startup-screen t)
+;; NO JUNK
+(setq auto-save-file-name-transforms `((".*" ,temporary-file-directory t))
+      backup-directory-alist `((".*" . ,temporary-file-directory)))
+
+;; EL-GET
+(add-to-list 'load-path "~/.emacs.d/el-get/el-get")
+
+(unless (require 'el-get nil 'noerror)
+  (with-current-buffer
+      (url-retrieve-synchronously
+       "https://raw.github.com/dimitri/el-get/master/el-get-install.el")
+    (goto-char (point-max))
+    (eval-print-last-sexp)))
+
+(add-to-list 'el-get-recipe-path "~/.emacs.d/el-get-user/recipes")
+(el-get 'sync)
+
+;; Other
 (require 'package)
 (add-to-list 'package-archives
              '("marmalade" . "http://marmalade-repo.org/packages/") t)
@@ -15,7 +37,7 @@
 (defvar packages (append common-packages ui-packages)
   "Combined list of packes to be installed")
 
-(dolist (p packages)
-  (when (not (package-installed-p p))
-    (message "package not installed: %s" p)
-    (package-install p)))
+;; (dolist (p packages)
+;;   (when (not (package-installed-p p))
+;;     (message "package not installed: %s" p)
+;;     (package-install p)))
